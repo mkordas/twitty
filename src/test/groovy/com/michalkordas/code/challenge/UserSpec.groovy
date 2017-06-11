ead172f351a41e199b8f38e84a6be72cd4f8bf3f
Michal Kordas <kordas.michal@gmail.com>
2017-06-11 14:20:14 +0200

    Add test for message posting

--- src/test/groovy/com/michalkordas/code/challenge/UserSpec.groovy
+++ src/test/groovy/com/michalkordas/code/challenge/UserSpec.groovy
@@ -11,4 +11,15 @@ class UserSpec extends Specification {
         expect:
         new User(sampleName).name() == sampleName
     }
+
+    def "can post a message"() {
+        given:
+        User user = new User("User")
+
+        when:
+        user.postMessage("some short message")
+
+        then:
+        noExceptionThrown()
+    }
 }

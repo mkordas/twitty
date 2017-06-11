0bdb41bee0156fd6aa2cef9e02ce446424d8c566
Michal Kordas <kordas.michal@gmail.com>
2017-06-11 14:28:19 +0200

    Implement length validation

--- src/main/java/com/michalkordas/code/challenge/User.java
+++ src/main/java/com/michalkordas/code/challenge/User.java
@@ -1,6 +1,8 @@
 package com.michalkordas.code.challenge;
 
 class User {
+    private static final int MAX_MESSAGE_LENGTH = 140;
+
     private final String name;
 
     User(String name) {
@@ -12,5 +14,10 @@ class User {
     }
 
     void postMessage(String message) {
+        if (message.length() > MAX_MESSAGE_LENGTH) {
+            throw new IllegalArgumentException(
+                "You cannot post message over 140 chars"
+            );
+        }
     }
 }

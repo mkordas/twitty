b073542895f707d1f12ee9ce32914e28ad8ec4b8
Michal Kordas <kordas.michal@gmail.com>
2017-06-12 08:32:05 +0200

    Add test for a wall

---
+++ src/test/groovy/com/michalkordas/code/challenge/WallSpec.groovy
@@ -0,0 +1,20 @@
+package com.michalkordas.code.challenge
+
+import spock.lang.Specification
+
+class WallSpec extends Specification {
+
+    def "displays posts in reverse chronological way"() {
+        given:
+        Wall wall = new Wall()
+        String first = 'first'
+        String second = 'second'
+
+        when:
+        wall.add(first)
+        wall.add(second)
+
+        then:
+        wall.messages() == [second, first]
+    }
+}

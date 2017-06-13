f5fde51593123c061164f897674aadc91270c5fd
Michal Kordas <kordas.michal@gmail.com>
2017-06-14 00:34:00 +0200

    Add test for Timeline

---
+++ src/test/groovy/com/michalkordas/code/challenge/TimelineSpec.groovy
@@ -0,0 +1,23 @@
+package com.michalkordas.code.challenge
+
+import spock.lang.Specification
+
+class TimelineSpec extends Specification {
+
+    def "returns messages in reverse chronological way"() {
+        given:
+        User firstUser = new User('First')
+        User secondUser = new User('Second')
+        Timeline timeline = new Timeline([firstUser, secondUser])
+        String firstMessage = 'first'
+        String secondMessage = 'second'
+        String thirdMessage = 'third'
+
+        secondUser.postMessage(firstMessage)
+        firstUser.postMessage(secondMessage)
+        secondUser.postMessage(thirdMessage)
+
+        expect:
+        timeline.messages() == [firstMessage, secondMessage, thirdMessage]
+    }
+}
